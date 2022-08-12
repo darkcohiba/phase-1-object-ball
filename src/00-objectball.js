a6512994a388608219ec6bc24828430364fa892a
Sam <samuel.waters@gmail.com>
2022-08-12 11:19:55 -0600

    teaching 2

---
+++ src/00-objectball.js
@@ -0,0 +1,25 @@
+function gameObject(){
+    return {
+        "home":{
+            teamName:"Brooklyn Nets",
+            colors: ["Black","White"],
+            players:{ "Alan Anderson":{
+                number: 0,
+                shoe: 16,
+                points: 22,
+                rebounds: 12,
+                assists: 12,
+
+            }
+
+            }
+        },
+        "away":{
+            teamName:"Charlotte Hornets",
+            colors: ["",""],
+            players:{
+
+            }
+        }
+    }
+}
